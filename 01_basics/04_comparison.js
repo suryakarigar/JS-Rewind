5f33e2cfa6bb8a35709b063a0c9db9e3cc882856
Surya-Karigar <suryakarigar.cv@gmail.com>
2024-02-01 18:36:05 +0530

    comparison and strict check

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,32 @@
+// console.log( 2 > 1 );
+// console.log( 2 >= 1 );
+// console.log( 2 < 1 );
+// console.log( 2 <= 1 );
+// console.log( 2 != 1 );
+// console.log( 2 == 1 );
+// console.log( 2 === 1 );
+
+// console.log( "2" > 1 );
+// console.log( 2 > "1" );
+// These types of conversion can result in faulty output. Make sure to compare the same datatype (recommended)
+
+// console.log( null > 0 );
+// console.log( null < 0 );
+// console.log( null >= 0 );
+// console.log( null <= 0 );
+// console.log( null == 0 );
+
+// console.log( undefined > 0 );
+// console.log( undefined < 0 );
+// console.log( undefined >= 0 );
+// console.log( undefined <= 0 );
+// console.log( undefined == 0 );
+
+/* --- STRICT CHECK --- */
+// console.log("2" == 2);   (ignores datatype)
+// console.log("2" === 2);  (checks datatype)
+
+// console.log( 2 === 2 );
+// console.log( 2 === "2" );
+// console.log( "2" === "2" ); 
+// console.log( "2" === 2 );
