c43c87cae2a05cab3dc84a793311886e6bac0de6
Surya-Karigar <suryakarigar.cv@gmail.com>
2024-02-01 15:41:17 +0530

    data types explained

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS Code as newer version
+
+// alert(3 + 6) //We're using Node.js and it doesn't work here 😉
+
+// console.log(6 + 3); console.log(5 + 4) // Bad practice for code readability
+
+// console.log(6 + 3)
+// console.log(5 + 4) // Good practice for code readability
+
+/* dataTypes */
+/* ---------------------------------------------- */
+/* ----> PRIMITIVE DATA TYPES <---- */
+// number       =>  2 (range 2 to power of 53)
+// bigint       =>  12316546198461946519846198465168416984219865189498.............
+// string       =>  "surya karigar" (anything inside " ")
+// boolean      =>  true/false (credit card details, data fetch, available, exists or not)
+// null         =>  (stand alone value but, data type also [object])
+// undefined    => e.g. let user; means value is not assigned
+// symbol       => to find uniqueness (will use in React project) Tools like figma use it heavily
+
+/* ----> NON-PRIMITIVE DATA TYPES <---- */
+// object
+
