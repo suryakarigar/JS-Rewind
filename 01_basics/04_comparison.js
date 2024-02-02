fa6164942a45116fe832eb685647cfd708460c40
Surya-Karigar <suryakarigar.cv@gmail.com>
2024-02-02 14:00:45 +0530

    Comment added

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -8,7 +8,7 @@
 
 // console.log( "2" > 1 );
 // console.log( 2 > "1" );
-// These types of conversion can result in faulty output. Make sure to compare the same datatype (recommended)
+// These types of conversion can result in faulty output. Never compare same datatypes (recommended)
 
 // console.log( null > 0 );
 // console.log( null < 0 );
@@ -23,10 +23,10 @@
 // console.log( undefined == 0 );
 
 /* --- STRICT CHECK --- */
-// console.log("2" == 2);   (ignores datatype)
-// console.log("2" === 2);  (checks datatype)
+// console.log("2" == 2);   (== ignores datatype in comparison)
+// console.log("2" === 2);  (=== stricktly checks datatype in comparison)
 
-// console.log( 2 === 2 );
-// console.log( 2 === "2" );
-// console.log( "2" === "2" ); 
-// console.log( "2" === 2 );
+console.log( 2 === 2 );         // true
+console.log( 2 === "2" );       // false
+console.log( "2" === "2" );     // true
+console.log( "2" === 2 );       // false
